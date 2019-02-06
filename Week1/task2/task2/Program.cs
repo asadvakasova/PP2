0480f3702acbc6392bec2bdf6745a91fb2ce5406
asadvakasova <01.sadvakassova@gmail.com>
2019-02-06 21:02:08 +0300

    week1

---
+++ Week1/task2/task2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    class Student
+    {
+        string name;//создаем строку под названием name
+        string id;//создаем другую строку под названием id
+        int Year = 1;//создаем переменную integer year равный одному
+        public Student(string name, string id)//создаем конструктор с двумя параметрами name и id
+        {
+            this.name = name;
+            this.id = id;
+        }
+        public void NewYear()//создаем конструктор без параметров
+        {
+            while (Year <= 4)
+            {
+                Console.WriteLine(name + " " + id + " " + Year);
+                Year++;
+            }//пока год обучения меньше или равен 4, то выводим имя, и год обучения. И после каждого выхода увеличиваем год на 1
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student("Assem Sadvakassova", "18BD555555");//создаем нового студента и задаем начальные данные
+            s1.NewYear();// здесь вызываем функцию, которая выводит
+        }
+    }
+}
