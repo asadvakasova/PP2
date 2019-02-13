2fc31470cbbaf8e0fc519c21139856f78f75a4b3
asadvakasova <01.sadvakassova@gmail.com>
2019-02-13 07:19:51 +0300

    Week2

---
+++ Week2/task3/task3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace task3
+{
+    class Program
+    {
+        public static void probel(int num)//создаем функцию, которая осталяет пробелы между уровнями папок 
+        {
+            for(int i=0; i<num; i++)
+            {
+                Console.Write("   ");
+            }
+        }
+        public static void Ex(DirectoryInfo dir, int a)//создаем функцию, которая выводит названия и все содержимое в папке 
+        {
+            foreach(FileInfo f in dir.GetFiles())//для файлов
+            {
+                probel(a);
+                Console.WriteLine(f.Name);
+            }
+            foreach(DirectoryInfo d in dir.GetDirectories())//для папок
+            {
+                probel(a);
+                Console.WriteLine(d.Name);
+                Ex(d, a + 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"C:\Users\Лох№1\Desktop\PP2");
+            Ex(dir, 0);//вызываем функцию
+            
+        }
+    }
+}
---
+++ Week2/task4/task4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "tea.txt";//имя файла, который я хочу создать
+            string PathString = @"C:\Users\Лох№1\Desktop\Week2";//и где я хочу его создать
+            PathString = Path.Combine(PathString, fileName);//нужно скомбинировать их пути
+            FileStream fs = File.Create(PathString); // и создаем файл
+            fs.Close();//нужно чтобы прекратил работу
+
+            string PathString2 = @"C:\Users\Лох№1\Desktop\Week2\task4";//путь куда мне нужно скопировать файл
+            string destFile = System.IO.Path.Combine(PathString2, fileName);//скомбинируем путь
+            File.Copy(PathString, destFile, true);//копируем туда
+            File.Delete(PathString);//и удаляем оригинал
+        }
+    }
+}
