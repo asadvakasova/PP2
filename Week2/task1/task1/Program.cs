58ae5ff3e47570a04c2aad0682b19fa5ddecaae5
asadvakasova <01.sadvakassova@gmail.com>
2019-02-13 05:49:06 +0300

    Week2

---
+++ Week2/task1/task1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace task1
+{
+    class Program
+
+    {
+        public static bool Ex(string s)//создаем функцию, которая проверяет строку на палиндром
+        {
+            for (int i = 0; i < s.Length / 2; i++)//до середины 
+            {
+                if (s[i] != s[s.Length - i - 1])//проверяет соответствующие элементы на палиндром 
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+}
+
+public static string Ex1()//создаем функцию, которая читает строку из файла 
+{
+    StreamReader sr = new StreamReader("input.txt");
+    string s = sr.ReadToEnd();
+    sr.Close();
+    return s;
+}
+
+
+        static void Main(string[] args)
+        {
+              string s = Ex1();
+              StreamReader sw = new StreamWriter("output.txt");//выводим ответ в другом файле
+    if (Ex(s) == true)
+    {
+        sw.WriteLine("yes");
+    }
+        else { sw.WriteLine("no");
+        }
+    sw.Close();
+
+        }
+    }
+}
---
+++ Week2/task2/task2/Program.cs
@@ -0,0 +1,55 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace task2
+{
+    class Program
+    {
+        public static bool Ex(int a)//создаем функцию, которая проверяет является ли число простым или нет
+        {
+            int k = 0;
+            for(int j = 1; j<=a; j++)
+            {
+                if(a % j == 0)
+                {
+                    k++;
+                }
+            }
+            if(k == 2)
+            {
+                return true;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public static string Read()//создаем функцию, чтобы прочесть числа из файла
+        {
+            StreamReader sr = new StreamReader("input.txt");
+            string s = sr.ReadToEnd();
+            sr.Close();
+            return s;
+        }
+ 
+        static void Main(string[] args)
+        {
+            string s = Read();
+            string[] arr = s.Split();//выводим все простые числа в другом файле
+            StreamWriter sw = new StreamWriter("output.txt");
+            for(int i = 0; i< arr.Lenght; i++)
+            {
+                if(Ex(int.Parse(arr[i])) == true)
+                {
+                    sw.Write(arr[i] + " ");
+                }
+            }
+            sw.Close();
+        }
+    }
+}
